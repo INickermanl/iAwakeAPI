0cb78d0998fc23ae48469260550a5829b63877b4
Nikolay Pluzhnik <kolya.pluzhnik@gmail.com>
2020-06-04 17:55:26 +0300

    init

---
+++ app/build.gradle.kts
@@ -0,0 +1,98 @@
+import Apps.versionCode
+import Apps.versionName
+import org.apache.commons.logging.LogFactory.release
+import org.gradle.internal.impldep.com.amazonaws.PredefinedClientConfigurations.defaultConfig
+import org.jetbrains.kotlin.konan.properties.Properties
+import org.jetbrains.kotlin.storage.CacheResetOnProcessCanceled
+import org.jetbrains.kotlin.storage.CacheResetOnProcessCanceled.enabled
+import com.android.build.gradle.internal.api.BaseVariantOutputImpl
+
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    id("kotlin-android-extensions")
+    kotlin("kapt")
+}
+
+android {
+    compileSdkVersion(Apps.compileSdk)
+
+    defaultConfig {
+        minSdkVersion(Apps.minSdk)
+        targetSdkVersion(Apps.targetSdk)
+        versionCode = Apps.versionCode
+        versionName = Apps.versionName
+        multiDexEnabled = true
+        setProperty("archivesBaseName", "$applicationId-v$versionName($versionCode)")
+        resConfigs("en")
+        ndk {
+            abiFilters("armeabi-v7a", "x86", "arm64-v8a", "x86_64")
+        }
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    bundle {
+        language { enableSplit = true }
+        density { enableSplit = true }
+        abi { enableSplit = true }
+    }
+
+    lintOptions {
+        isAbortOnError = false
+        isIgnoreWarnings = true
+        isQuiet = true
+    }
+
+    packagingOptions {
+        exclude("META-INF/notice.txt")
+    }
+
+    buildTypes {
+
+    }
+
+    testOptions {
+        unitTests.isReturnDefaultValues = true
+    }
+
+    dependencies {
+        implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+        implementation(Libs.kotlin)
+        implementation(Libs.appcompat)
+        implementation(Libs.coreKotlin)
+        implementation(Libs.constraintVersion)
+        testImplementation(TestLibs.junit)
+        androidTestImplementation(TestLibs.junitExt)
+        androidTestImplementation(TestLibs.espresso)
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+
+    buildFeatures {
+        dataBinding = true
+    }
+
+    kapt {
+        javacOptions {
+            option("-Xmaxerrs", 99999)
+        }
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_1_8.toString()
+        freeCompilerArgs = freeCompilerArgs + "-Xopt-in=kotlin.RequiresOptIn"
+    }
+
+    buildTypes {
+        get("debug").apply {
+            buildConfigField("Boolean", "BEBUG_BUILD_TYPE", "true")
+        }
+        get("release").apply {
+            buildConfigField("Boolean", "BEBUG_BUILD_TYPE", "false")
+        }
+    }
+}
---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    repositories {
+        google()
+        jcenter()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:4.0.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        jcenter()
+        maven { url = uri(Maven.uri) }
+    }
+}
+
+tasks.register("clean",Delete::class){
+    delete(rootProject.buildDir)
+}
